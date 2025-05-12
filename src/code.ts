99077d3d79322b639a8868ef228cfe03b7dbe9f5
Jimmy Mayta <jimmyymaytaj@gmail.com>
2025-05-12 01:56:29 -0400

    Update code.ts

--- src/code.ts
+++ src/code.ts
@@ -1,19 +1,17 @@
+const random = (): number => {
+  const data: number[] = [
+    Math.round(Math.random() * (9 - 1)) + 1,
+    Math.round(Math.random() * (9 - 1)) + 1,
+  ].sort();
+  return Math.round(Math.random() * (data[1] - data[0])) + data[0];
+};
+
 const num = (n: number, d: number): string => {
   return ('0'.repeat(Math.abs(n - d.toString().length)) + d).slice(-n);
 };
 
-const randomnumber = (): string => {
-  const a = Math.floor(Math.random() * 9) + 1;
-  const b = Math.floor(Math.random() * 9) + 1;
-  return String(a > b ? Math.floor(Math.random() * (a - b + 1)) + b : Math.floor(Math.random() * (b - a + 1)) + a);
-}
-
-const random = (n: number): string => {
-  let r = ''
-  for (let i = 0; i < n; i++) {
-    r += randomnumber();
-  }
-  return r
+const number_random = (n: number): string => {
+  return Array.from({ length: n }, random).join('');
 };
 
 const code = (): string => {
@@ -26,7 +24,7 @@ const code = (): string => {
   const minutes = newdate.getMinutes();
   const seconds = newdate.getSeconds();
   const milliseconds = newdate.getMilliseconds();
-  return `${num(4, year)}${num(2, month)}${num(2, day)}${num(2, hours)}${num(2, minutes)}${num(2, seconds)}${num(4, milliseconds)}${num(13, datenow)}${random(3)}`;
+  return `${num(4, year)}-${num(2, month)}-${num(2, day)} ${num(2, hours)}:${num(2, minutes)}:${num(2, seconds)}.${num(4, milliseconds)} ${num(13, datenow)} ${number_random(3)}`;
 };
 
 export default code;
